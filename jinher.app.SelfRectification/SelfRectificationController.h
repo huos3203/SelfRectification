46525dab097f8c22bf95c0e100100a7a5a6a388b
huos3203 <724987481@qq.com>
2018-12-10 21:16:32 +0800

    搭建项目

---
+++ jinher.app.SelfRectification/SelfRectificationController.h
@@ -0,0 +1,17 @@
+//
+//  SelfRectificationController.h
+//  com.jinher.SelfRectification
+//
+//  Created by admin on 2018/12/10.
+//  Copyright © 2018年 Jinher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SelfRectificationController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ jinher.app.SelfRectification/SelfRectificationController.m
@@ -0,0 +1,32 @@
+//
+//  SelfRectificationController.m
+//  com.jinher.SelfRectification
+//
+//  Created by admin on 2018/12/10.
+//  Copyright © 2018年 Jinher. All rights reserved.
+//
+
+#import "SelfRectificationController.h"
+
+@interface SelfRectificationController ()
+
+@end
+
+@implementation SelfRectificationController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
