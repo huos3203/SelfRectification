5f87c97a280da305a741395aad9bee11f4586d03
huos3203 <724987481@qq.com>
2018-12-10 14:13:50 +0800

    Initial Commit

---
+++ com.jinher.SelfRectification/com_jinher_SelfRectification.h
@@ -0,0 +1,19 @@
+//
+//  com_jinher_SelfRectification.h
+//  com.jinher.SelfRectification
+//
+//  Created by admin on 2018/12/10.
+//  Copyright © 2018年 Jinher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for com_jinher_SelfRectification.
+FOUNDATION_EXPORT double com_jinher_SelfRectificationVersionNumber;
+
+//! Project version string for com_jinher_SelfRectification.
+FOUNDATION_EXPORT const unsigned char com_jinher_SelfRectificationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <com_jinher_SelfRectification/PublicHeader.h>
+
+
